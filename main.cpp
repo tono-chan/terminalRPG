52b253a45d89bfd8d2e3998433798364c226f4ee
Youhei Tonomura <ot23ytonomura@ec.usp.ac.jp>
2017-05-08 23:19:58 +0900

    make clion project

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by youhei on 17/05/08.
+//
+
+#include <iostream>
+
+int main( int args, char argv[]) {
+    std::cout << "Hello World!" << std::endl;
+
+
+
+
+}
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by youhei on 17/05/08.
+//
+
+#ifndef PROJECT_MAIN_H
+#define PROJECT_MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //PROJECT_MAIN_H
