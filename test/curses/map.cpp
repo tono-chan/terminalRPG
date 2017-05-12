c11bcc540b322649d13fc7ae8db095dad196156c
Youhei Tonomura <ot23ytonomura@ec.usp.ac.jp>
2017-05-12 14:43:16 +0900

    map移動のテストプログラム

--- test/curses/map.cpp
+++ test/curses/map.cpp
@@ -14,7 +14,7 @@ int main (void)
 {
 
   setlocale (LC_ALL, "");
-  std::string player_icon = "あ";
+  std::string player_icon = "勇";
 //  std::string player_icon = "\u265E";
   std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> converter;
   string_util sutil (player_icon);
@@ -22,21 +22,38 @@ int main (void)
 
   int x, y, w, h;
   int key;
-
   initscr ();
   noecho ();
   cbreak ();
   curs_set (0);
   keypad (stdscr, TRUE);
+
   getmaxyx(stdscr, h, w);
-  x=10;
-  y=10;
+  x=0;
+  y=0;
+
+
+  int map_h = 10;
+  int map_w = 20;
+  std::string word = "hello world";
+  WINDOW *mapOut = newpad (map_h + h, map_w + w );
+  WINDOW *mapFrame = subwin(mapOut, map_h+2, map_w+2, h/2-1, w/2-1);
+  WINDOW *map = subwin (mapOut, map_h, map_w, h/2, w/2 );
+  box( mapFrame , '|','-' );
+  wmove(map , map_h/2, map_w/2 - word.length ()/2 );
+  waddstr (map ,word.c_str ());
+
+
   while (1)
     {
-      erase ();
-      move (y, x);
-      addstr( player_icon.c_str () );
-      refresh ();
+      wclear(map);
+      wmove(map,4,4);
+      waddstr(map, "敵");
+      wmove(map,y,x);
+      waddstr(map ,player_icon.c_str ());
+      wrefresh (map);
+      prefresh (mapOut ,y, x, 0, 0, h-1, w-1 );
+      touchwin (mapOut);
 
       key = getch();
       if (key == 'q') break;
@@ -48,7 +65,7 @@ int main (void)
               y--;
               }
           break;
-          case KEY_DOWN:if(y + 1 < h) { y++;}
+          case KEY_DOWN:if(y + 1 < map_h) { y++;}
           break;
           case KEY_LEFT:
             if (x > 0)
@@ -57,7 +74,7 @@ int main (void)
               }
           break;
           case KEY_RIGHT:
-            if (x + player_width < w )
+            if (x + player_width < map_w )
               {
                 x++;
               }
